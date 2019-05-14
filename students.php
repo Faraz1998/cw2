2257e6679e2e3919ab55630bde8ca4ac1555d0b4
Faraz1998 <44966455+Faraz1998@users.noreply.github.com>
2019-05-14 14:09:22 +0100

    test

--- students.php
+++ students.php
@@ -1,1 +1,38 @@
-sd
+<?php
+
+   include("_includes/config.inc");
+   include("_includes/dbconnect.inc");
+   include("_includes/functions.inc");
+
+
+   // check logged in
+   if (isset($_SESSION['id'])) {
+
+      echo template("templates/partials/header.php");
+      echo template("templates/partials/nav.php");
+
+      // Build SQL statment that selects a student's modules
+      $sql = "select * from student";
+
+      $result = mysqli_query($conn,$sql);
+
+      // prepare page content
+      $data['content'] .= "<table border='1'>";
+      $data['content'] .= "<tr><th>ModuleCode</th><th>Name</th><th>Level</th></tr>";
+      // Display the modules within the html table
+      while($row = mysqli_fetch_array($result)) {
+         $data['content'] .= "<tr><td> $row[modulecode] </td><td> $row[name] </td>";
+         $data['content'] .= "<td> $row[level] </td></tr>";
+      }
+      $data['content'] .= "</table>";
+
+      // render the template
+      echo template("templates/default.php", $data);
+
+   } else {
+      header("Location: index.php");
+   }
+
+   echo template("templates/partials/footer.php");
+
+?>
