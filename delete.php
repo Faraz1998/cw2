12e347a2af6decb9bf9c14ae0681b1e1e1a65a1d
Faraz1998 <44966455+Faraz1998@users.noreply.github.com>
2019-05-20 10:07:53 +0100

    p5

--- delete.php
+++ delete.php
@@ -1,15 +1,10 @@
 <?php
-
    include("_includes/config.inc");
    include("_includes/dbconnect.inc");
    include("_includes/functions.inc");
-
-if(isset ($_POST['delete' ]))
-{
-  $chkarr = $_POST ['checkbox'];
-foreach($chkarr as $studentid) {
-  mysqli_query (com, "Delete From student where studentid = $studentid");
-}
-header("Location:students.php");
-}
+if(isset ($_POST['delete']))
+{ $Ar = $_POST['checkbox'];
+foreach($Ar as $studentid)
+ {mysqli_query ($conn,  "Delete From student where studentid = $studentid"); }
+header("Location:students.php"); }
 ?>
--- students.php
+++ students.php
@@ -1,34 +1,34 @@
 <form method="post" action="delete.php">
-<?php
-   include("_includes/config.inc");
-   include("_includes/dbconnect.inc");
-   include("_includes/functions.inc");
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+
+<?php include("_includes/config.inc");
+      include("_includes/dbconnect.inc");
+      include("_includes/functions.inc");
    // check logged in
    if (isset($_SESSION['id'])) {
       echo template("templates/partials/header.php");
       echo template("templates/partials/nav.php");
-      // Build SQL statment that selects a student's modules
+
       $sql = "select * from student";
       $result = mysqli_query($conn,$sql);
 
-      // prepare page content
       $data['content'] .= "<table border='1'>";
-      $data['content'] .= "<tr><th colspan='9' align='center'>Students</th></tr>";
-      $data['content'] .= "<tr><th></th><th>ID</th><th>DOB</th><th>FirstName</th><th>Surname</th><th>House</th><th>Town</th><th>County</th><th>Country</th></tr>";
-      // Display the modules within the html table
+      $data['content'] .= "<tr><th colspan='10' align='center'>Student Details</th></tr>";
+      $data['content'] .= "<tr><th></th><th>ID</th><th>DOB</th><th>FirstName</th><th>Surname</th>
+      <th>House</th><th>Town</th><th>County</th><th>Country</th><th>Postcode</th></tr>";
+
       while($row = mysqli_fetch_array($result)) {
-         $data['content'] .= "<tr><td><input type='checkbox' name='checkbox[]' value='".$row['studentid']."'> </td> <td> $row[studentid] </td><td> $row[dob] </td><td> $row[firstname] </td><td> $row[lastname] </td><td> $row[house] </td><td> $row[town] </td><td> $row[county] </td><td> $row[country] </td></tr>";
-      }
+         $data['content'] .= "<tr><td><input type='checkbox' name='checkbox[]' value='".$row['studentid']."'> </td>
+          <td> $row[studentid] </td><td> $row[dob] </td><td> $row[firstname] </td><td> $row[lastname] </td><td> $row[house] </td>
+          <td> $row[town] </td><td> $row[county] </td><td> $row[country] </td><td>$row[postcode]</td></tr>";  }
       $data['content'] .= "</table>";
-      // render the template
+
       echo template("templates/default.php", $data);
-
    } else {
-      header("Location: index.php");
-   }
-
+      header("Location: index.php"); }
    echo template("templates/partials/footer.php");
-
 ?>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
 <input type="submit" name="delete" id="delete" value="Delete Records">
 </form>
